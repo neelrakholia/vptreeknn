4ce52356ad466e2fd9606eaaeeb56e6b6408a855
neelrakholia <neel.rakholia@gmail.com>
2015-07-29 19:51:54 -0500

    Added formatted rbf distance function

---
+++ src/rbf.m
@@ -0,0 +1,25 @@
+function d = rbf(X, x, sigma)
+%--------------------------------------------------------------------------
+% RBF Evaluates pairwise RBF kernel distances between points in 2 arrays 
+% strings out of it
+%   Input 
+%       X - Array of points
+%       x - Array of points
+%       sigma - Kernel bandwidth parameter
+%
+%   Output 
+%       d - evaluated kernel value
+%--------------------------------------------------------------------------
+% evaluate euclidean part
+[~,N] = size(X);
+[~,n] = size(x);
+X2 = sum(X.^2,1);
+x2 = sum(x.^2,1);
+dotProd = X'*x;
+d = repmat(x2,N,1)+repmat(X2',1,n)-2*dotProd;
+
+% evaluate kernel
+d = exp(-d/(2*sigma^2));
+d = sqrt(-2*d + 2);
+d = d./(d + 1);
+end
