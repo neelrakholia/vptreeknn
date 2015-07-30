59ba05822a52152d99535499ece4872c1335c133
neelrakholia <neel.rakholia@gmail.com>
2015-07-30 13:22:39 -0500

    Updated formatting
    Conflicts:
    	src/ssk_dyn.m
    	src/test_ssk_dyn.m

--- src/test_ssk_dyn.m
+++ src/test_ssk_dyn.m
@@ -6,25 +6,18 @@ s2 = 'wisdom is organized life';
 lambda = 0.5;
 
 for n = 1:6
-
-<<<<<<< HEAD
-    tic 
+    
+    tic
     k_ssk = ssk_fast(s1, s2, n, lambda);
     toc
     fprintf('ssk_fast: %g\n', k_ssk);
-=======
-     tic 
-     k_ssk = ssk_fast(s1, s2, n, lambda);
-     toc
-     fprintf('ssk_fast: %g\n', k_ssk);
->>>>>>> origin/master
-
-
+    
+    
     tic
     k_dyn = ssk_dyn(s1, s2, n, lambda);
     toc
     fprintf('ssk_dyn: %g\n', k_dyn);
-
+    
 end
 
 
