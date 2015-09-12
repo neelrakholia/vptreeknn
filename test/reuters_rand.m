c1fcf11d358ba1dce7d393d3924d63c8adb2df7a
neelrakholia <neel.rakholia@gmail.com>
2015-09-12 09:56:22 -0400

    Updated parameters

--- test/reuters_rand.m
+++ test/reuters_rand.m
@@ -22,15 +22,15 @@ data = cat(2,data,earn);
 data = cat(2,data,acq);
 
 % get labels for data
-% crude = 0
-% livestock = 1
-% earn = 2
-% labels = ones(length(data),1);
-% labels(1:length(crude)) = 0;
-% labels(length(crude) + length(livestock) + 1:...
-%     length(crude) + length(livestock) + length(earn)) = 2;
-% labels(length(crude) + length(livestock) + length(earn) + 1:...
-%     end) = 3;
+% crude = 0;
+% livestock = 1;
+% earn = 2;
+labels = ones(length(data),1);
+labels(1:length(crude)) = 0;
+labels(length(crude) + length(livestock) + 1:...
+    length(crude) + length(livestock) + length(earn)) = 2;
+labels(length(crude) + length(livestock) + length(earn) + 1:...
+    end) = 3;
 
 % percentage split for test and train
 per = 0.2;
@@ -40,9 +40,9 @@ ind = randperm(length(data));
 train_ind = ind(1:floor(length(data)*per));
 test_ind = ind(floor(length(data)*per)+1:end);
 train = data(train_ind);
-% train_lab = labels(train_ind);
+train_lab = labels(train_ind);
 test = data(test_ind);
-% test_lab = labels(test_ind);
+test_lab = labels(test_ind);
 
 %%
 
@@ -61,8 +61,8 @@ lambda = 0.5;
 order = 3;
 
 % run random NN algo and see how it compares with quad search
-% randomvp(train(1:num_train), test(1:num_test), num_train, num_test, 10, 3, lambda, ...
-%     40, 5, 1, train_lab(1:num_train), test_lab(1:num_test))
+randomvp(train(1:num_train), test(1:num_test), num_train, num_test, 2, 3, lambda, ...
+     5, 2, 1, train_lab(1:num_train), test_lab(1:num_test))
 
 tic
 K = wsk_mex(train(1:num_train), test(1:num_test), lambda, order);
