9d9339ea8e2c3f7d5500dd982b9642f696bd19b5
Bill March <march@ices.utexas.edu>
2015-10-07 14:34:21 -0500

    updating klsh

---
+++ klsh/klsh_test.m
@@ -0,0 +1,50 @@
+
+addpath '~/vptreeknn/src'
+addpath '~/vptreeknn/src/kernels'
+
+clear; clc; close all;
+
+% for debugging
+rng(1);
+
+% read train
+filename = '~/covtype.libsvm.trn.X.bin';
+num_references = 499999;
+dim = 54;
+train = binread_array(filename, num_references*dim);
+train = reshape(train, dim, num_references);
+
+% read test
+filename = '~/covtype.libsvm.tst.X.bin';
+num_queries = 80012;
+test = binread_array(filename, num_queries*dim);
+test = reshape(test, dim, num_queries);
+
+% sample data
+num_references = 10000;
+num_queries = 100;
+train = datasample(train, num_references, 2, 'Replace', false);
+test = datasample(test, num_queries, 2, 'Replace', false);
+
+rng('shuffle')
+
+%%
+
+% parameters
+p = 300;
+b = 30;
+B = 15;
+k = 10;
+
+epsilon = 1;
+M = floor(num_references^(1/(1+epsilon)));
+
+h = 0.22;
+kernelfun = @(q,r) rbf(q, r, h);
+
+[acc, dists] = klsh(test, train, k, b, B, M, p, kernelfun);
+
+
+
+
+
